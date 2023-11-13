8b6dcd7c566d33bb022a9ff57a88257c7a3f3de2
Simran shaikh <simranshaikh30158@gmail.com>
2023-11-13 23:49:03 +0530

    Basic pratice of java

---
+++ Basicp.java
@@ -0,0 +1,12 @@
+public class Basicp {
+
+    public static void main(String[] args) {
+        System.out.println("1) this is simple text");
+        System.out.println("2) using println, is good as breaks line each time");
+
+        System.out.print("3) without println ");
+        System.out.print("4) code doesnt change line automatically");
+        System.out.println(" how to comment in java");
+         System.out.println(" 5) Normally, we use // for short comments, and /* */ for longer.");
+    }
+}

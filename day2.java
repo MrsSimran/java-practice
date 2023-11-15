be28d97eaeb0106f67d990edc6346af35b36ba46
Simran shaikh <simranshaikh30158@gmail.com>
2023-11-16 01:22:41 +0530

    update

--- day2.java
+++ day2.java
@@ -27,5 +27,19 @@ public class day2 {
         c <<= 3;
         System.out.println(c);
 
+        int d = 5;
+        System.out.println(d > 3 && d < 10);
+
+        // returns true because 5 is greater than 3 AND 5 is less than 10
+
+        int e = 5;
+        System.out.println(e > 3 || e < 4);
+
+        // returns true because one of the conditions are true (5 is greater than 3, but
+        // 5 is not less than 4)
+
+        int f = 5;
+        System.out.println(!(f > 3 && f < 10));
+        // returns false because ! (not) is used to reverse the result
+
     }
-}

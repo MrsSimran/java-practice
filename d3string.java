cb6715f44502963a1af15797a471198b9e7066c7
Simran shaikh <simranshaikh30158@gmail.com>
2023-11-17 23:52:09 +0530

    string notes added

---
+++ d3string.java
@@ -0,0 +1,16 @@
+public class d3string {
+    public static void main(String [] args) {
+        String txt = "count lenght of letter";
+        System.out.println("the lenght of txt is:" + txt.length());
+
+         System.out.println("the lenght of txt is:" + txt.toUpperCase());
+          System.out.println("the lenght of txt is:" + txt.toLowerCase());
+
+          String text = "We are the so-called \"Vikings\" this make qutation to word.";
+         System.out.println(text);
+        String text2="The sequence \'  inserts a single\' quote in a string";
+     System.out.println(text2);
+ String text3 = "The sequence \\\\  inserts a single backslash in a string:";
+      System.out.println(text3);
+        }
+}

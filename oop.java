eee626a1319ea94e28ad2825692e9cb91fb04dd2
Simran shaikh <simranshaikh30158@gmail.com>
2023-11-20 23:50:38 +0530

    oop

---
+++ oop.java
@@ -0,0 +1,16 @@
+public class oop {
+    
+
+  
+        int x = 5;
+      
+        public static void main(String[] args) {
+          oop myObj1 = new oop();
+          oop myObj2 = new oop();
+          System.out.println(myObj1.x);
+          System.out.println(myObj2.x);
+        
+      
+        }
+
+}

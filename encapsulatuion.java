6a0e828d5fbb9439e7eaed742a2fef1a27043592
Simran shaikh <simranshaikh30158@gmail.com>
2023-11-25 00:16:03 +0530

    Day- 4

---
+++ encapsulatuion.java
@@ -0,0 +1,27 @@
+public class Main {
+  public static void main(String[] args) {
+    Person myObj = new Person();
+    myObj.setName("John");
+    System.out.println(myObj.getName());
+  }
+}
+
+
+
+
+
+   /** public class Person {
+        private String name;
+     
+        // Getter
+        public String getName() {
+          return name;
+        }
+     
+        // Setter
+        public void setName(String newName) {
+          this.name = newName;
+        }
+     }
+
+**/

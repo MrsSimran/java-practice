3a98b41582b4944dd5ee906d53ae80a6a76e670b
Simran shaikh <simranshaikh30158@gmail.com>
2023-12-10 00:05:48 +0530

    break/continue

---
+++ main.java
@@ -0,0 +1,28 @@
+public class Main {
+  public static void main(String[] args) {
+    int i = 0;
+    while (i < 10) {
+      if (i == 4) {
+        i++;
+        continue;
+      }
+      System.out.println(i);
+      i++;
+    }  
+  }
+}
+/*
+public class Main {
+  public static void main(String[] args) {
+    int i = 0;
+    while (i < 10) {
+      System.out.println(i);
+      i++;
+      if (i == 4) {
+        break;
+      }
+    }  
+  }
+}
+
+*/
